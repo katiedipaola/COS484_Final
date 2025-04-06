d46e4441c9d32233e9c8413dc5d0d369df80a3d5
katiedipaola <123992409+katiedipaola@users.noreply.github.com>
2025-04-06 19:39:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,49 +1,68 @@
 #!/bin/bash
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
+pip install pip==23.2.1
+pip install fairseq
+pip install subword-nmt torchmetrics
 
-# Create and activate a virtual environment
-PYTHON_CMD=$(command -v python3.9)
+git clone https://github.com/pytorch/fairseq.git
+cd fairseq
+pip install --editable ./
+pip install subword-nmt sacremoses
 
-if [ -z "$PYTHON_CMD" ]; then
-  echo "python3.9 not found. Please install it or use pyenv."
-  exit 1
-fi
+ln -s /content/fairseq/fairseq_cli/fairseq-preprocess /usr/local/bin/fairseq-preprocess
+chmod +x /usr/local/bin/fairseq-preprocess
 
-$PYTHON_CMD -m venv fairseq_env
-source fairseq_env/bin/activate
+cd fairseq
+bash examples/translation/prepare-iwslt14.sh
 
-# Upgrade pip to version 24
-pip install --upgrade pip==24.0
-pip install torch
-pip install "numpy<2.0" 
-pip install packaging
-pip install pyarrow subword-nmt sacremoses
+find fairseq -name "train.de"
+ls fairseq/iwslt14.tokenized.de-en/
 
-# Set CUDA_HOME to the correct location
-export CUDA_HOME="/usr/local/cuda"  # Replace with the actual path, e.g., /usr/local/cuda or $HOME/cuda
 
-# Update the PATH to include CUDA bin and lib64 directories
-export PATH="$CUDA_HOME/bin:$PATH"
-export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$LD_LIBRARY_PATH"
+# # Create and activate a virtual environment
+# PYTHON_CMD=$(command -v python3.9)
 
-# # Ensure wget is installed
-# if ! command -v wget &> /dev/null; then
-#     echo "wget not found, installing..."
-#     brew install wget
+# if [ -z "$PYTHON_CMD" ]; then
+#   echo "python3.9 not found. Please install it or use pyenv."
+#   exit 1
 # fi
 
-# Clone the Fairseq repository
-git clone https://github.com/pytorch/fairseq
-cd fairseq
-CFLAGS='-std=c++11' pip install .
+# $PYTHON_CMD -m venv fairseq_env
+# source fairseq_env/bin/activate
 
-# CFLAGS="-stdlib=libc++" pip install --editable ./
+# # Upgrade pip to version 24
+# pip install --upgrade pip==24.0
+# pip install torch
+# pip install "numpy<2.0" 
+# pip install packaging
+# pip install pyarrow subword-nmt sacremoses
 
-git clone https://github.com/NVIDIA/apex
-cd apex
-pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" \
-  --global-option="--deprecated_fused_adam" --global-option="--xentropy" --no-build-isolation\
-  --global-option="--fast_multihead_attn" ./
+# # Set CUDA_HOME to the correct location
+# export CUDA_HOME="/usr/local/cuda"  # Replace with the actual path, e.g., /usr/local/cuda or $HOME/cuda
 
-pip install pyarrow
-pip install subword-nmt
-pip install sacremoses
+# # Update the PATH to include CUDA bin and lib64 directories
+# export PATH="$CUDA_HOME/bin:$PATH"
+# export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$LD_LIBRARY_PATH"
+
+# # # Ensure wget is installed
+# # if ! command -v wget &> /dev/null; then
+# #     echo "wget not found, installing..."
+# #     brew install wget
+# # fi
+
+# # Clone the Fairseq repository
+# git clone https://github.com/pytorch/fairseq
+# cd fairseq
+# CFLAGS='-std=c++11' pip install .
+
+# # CFLAGS="-stdlib=libc++" pip install --editable ./
+
+# git clone https://github.com/NVIDIA/apex
+# cd apex
+# pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" \
+#   --global-option="--deprecated_fused_adam" --global-option="--xentropy" --no-build-isolation\
+#   --global-option="--fast_multihead_attn" ./
+
+# pip install pyarrow
+# pip install subword-nmt
+# pip install sacremoses
