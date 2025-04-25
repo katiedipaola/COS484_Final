dcfc112d904bf82650aeee467727abf4a3895fe8
OJLafond <115372464+OJLafond@users.noreply.github.com>
2025-04-25 11:30:57 -0400

    training on full model as well

--- training_split_data.py
+++ training_split_data.py
@@ -26,3 +26,25 @@ for i in range(10):
     ]
 
     subprocess.run(command)
+
+command = [
+    "fairseq-train",
+    "--arch", "transformer",
+    "--encoder-layers", "6", "--decoder-layers", "6",
+    "--encoder-embed-dim", "512", "--decoder-embed-dim", "512",
+    "--encoder-ffn-embed-dim", "1024", "--decoder-ffn-embed-dim", "1024",
+    "--encoder-attention-heads", "4", "--decoder-attention-heads", "4",
+    "--share-decoder-input-output-embed",
+    "--optimizer", "adam", "--adam-betas", "0.9, 0.98", "--clip-norm", "0.0",
+    "--lr", "5e-4", "--lr-scheduler", "inverse_sqrt", "--warmup-updates", "4000",
+    "--dropout", "0.3", "--criterion", "label_smoothed_cross_entropy", "--label-smoothing", "0.1",
+    "--max-tokens", "4096",
+    "--eval-bleu", "--eval-bleu-detok", "moses", "--eval-bleu-remove-bpe",
+    "--best-checkpoint-metric", "bleu", "--maximize-best-checkpoint-metric",
+    "--save-dir", f"checkpoints/full_model",
+    "--log-format", "simple", "--log-interval", "10",
+    "--valid-subset", "train",
+    f"data-bin/iwslt14_bpe_full",
+    "--max-epoch", "70"
+]
+subprocess.run(command)
