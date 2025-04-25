a91f0086a1a711d104c86644c2be3431f1ed7b9b
OJLafond <115372464+OJLafond@users.noreply.github.com>
2025-04-25 11:08:03 -0400

    Added generations for the full model

--- generate_predictions.py
+++ generate_predictions.py
@@ -15,3 +15,16 @@ for i in range(10):
     ]
 
     subprocess.run(command)
+
+# generate predictions for the full model used in algorithm 2
+command = [
+        "fairseq-generate",
+        f"data-bin/iwslt14_bpe_full",
+        "--path", f"checkpoints/full_model/checkpoint5.pt",
+        "--gen-subset", "train",
+        "--beam", "5",
+        "--remove-bpe",
+        "--batch-size", "32",
+        "--results-path", f"results/full_model"
+]
+subprocess.run(command)
